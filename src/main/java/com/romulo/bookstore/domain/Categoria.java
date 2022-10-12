237bb7873ffabcdb768e5082d7cc72015a2e4cbc
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-10-12 14:12:36 -0300

    fix: ajustes de fnalização no back

--- src/main/java/com/romulo/bookstore/domain/Categoria.java
+++ src/main/java/com/romulo/bookstore/domain/Categoria.java
@@ -10,6 +10,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -20,7 +23,13 @@ public class Categoria implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Campo NOME requerido")
+	@Length(min =3, max = 100, message = "O campo NOME deve ter entre 3 e 100 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRIÇÃO requerido")
+	@Length(min =3, max = 200, message = "O campo DESCRIÇÃO deve ter entre 3 e 200 caracteres")
 	private String descricao;
 
 	@OneToMany(mappedBy = "categoria")
--- src/main/java/com/romulo/bookstore/domain/Livro.java
+++ src/main/java/com/romulo/bookstore/domain/Livro.java
@@ -9,6 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -21,8 +24,17 @@ public class Livro implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Campo TITULO requerido")
+	@Length(min =3, max = 50, message = "O campo TITULO deve ter entre 3 e 50 caracteres")
 	private String titulo;
+	
+	@NotEmpty(message = "Campo NOME DO AUTOR requerido")
+	@Length(min =3, max = 50, message = "O campo NOME DO AUTOR deve ter entre 3 e 50 caracteres")
 	private String nomeAutor;
+	
+	@NotEmpty(message = "Campo TEXTO requerido")
+	@Length(min =10, max = 2000000, message = "O campo TEXTO deve ter entre 10 e 2.000.000 caracteres")
 	private String texto;
 
 	@JsonIgnore
