f9d8354d0e0c180b81ce9efbd800a140f11b165e
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-10-11 21:47:24 -0300

    feat: findById Categoria

--- src/main/java/com/romulo/bookstore/domain/Livro.java
+++ src/main/java/com/romulo/bookstore/domain/Livro.java
@@ -10,6 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_livro")
 public class Livro implements Serializable{
@@ -23,6 +25,7 @@ public class Livro implements Serializable{
 	private String nomeAutor;
 	private String texto;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
---
+++ src/main/java/com/romulo/bookstore/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.romulo.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.romulo.bookstore.domain.Categoria;
+import com.romulo.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
